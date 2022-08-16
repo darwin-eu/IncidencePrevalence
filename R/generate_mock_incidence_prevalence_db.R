a584dad71dc032d9ff6d13fa209b0fd0fee91759
edward-burn <9583964+edward-burn@users.noreply.github.com>
2022-08-16 12:21:34 +0200

    updates to mock db

--- R/generate_mock_incidence_prevalence_db.R
+++ R/generate_mock_incidence_prevalence_db.R
@@ -54,9 +54,10 @@
 generate_mock_incidence_prevalence_db <- function(person = NULL,
                                                   observation_period = NULL,
                                                   outcome = NULL,
-                                                  sample_size = 100,
-                                                  out_pre = 0.1,
+                                                  sample_size = 1,
+                                                  out_pre = 1,
                                                   seed = 444) {
+
   errorMessage <- checkmate::makeAssertCollection()
   checkmate::assert_tibble(person, null.ok = TRUE)
   checkmate::assert_tibble(observation_period, null.ok = TRUE)
@@ -66,84 +67,33 @@ generate_mock_incidence_prevalence_db <- function(person = NULL,
   checkmate::assert_int(seed, lower = 1)
   checkmate::reportAssertions(collection = errorMessage)
 
-  sample_size <- sample_size
-  Prevalance <- out_pre
-  unique_obs <- round(max(1, sample_size * 0.1), digits = 0)
   set.seed(seed)
+
+  if(is.null(person) | is.null(observation_period)){
   # person table
   id <- as.character(seq(1:sample_size))
   # person gender
-  gender_concept_list <- c("8507", "8532")
-  gender_id <-
-    sample(gender_concept_list, sample_size, replace = TRUE)
-
-  ##### person date of birth (DOB)####
-
-  DOB <-
-    sample(seq(as.Date("1920-01-01"), as.Date("2000-01-01"), by = "day"), sample_size, replace = TRUE) # creating a list of random person date of birth
-  # list for year, month, day
+  gender_id <- sample(c("8507", "8532"),
+                      sample_size, replace = TRUE)
+  # person date of birth
+  # random date of birth
+  DOB <- sample(seq(as.Date("1920-01-01"),
+                    as.Date("2000-01-01"),
+                    by = "day"),
+                sample_size,
+                replace = TRUE)
+  # year, month, day
   DOB_year <- as.numeric(format(DOB, "%Y"))
   DOB_month <- as.numeric(format(DOB, "%m"))
   DOB_day <- as.numeric(format(DOB, "%d"))
 
   # observation_period table
-  obs_id <-
-    as.character(seq(1:unique_obs)) # create a list of observational_period_id
+  # create a list of observational_period_id
   obs_start_date <-
-    sample(seq(as.Date("2005-01-01"), as.Date("2010-01-01"), by = "day"), unique_obs, replace = TRUE) # start date for the period
-  obs_end_date <-
-    sample(seq(as.Date(max(obs_start_date)), as.Date("2021-01-01"), by = "day"), length(obs_start_date), replace = TRUE) # end date for the period
-  obs_id_assign <-
-    sample(obs_id, sample_size, replace = TRUE) # assigning observational_period_id to person
-  obs_start_date_assign <-
-    obs_start_date[as.numeric(obs_id_assign)] # assigning start date to person
-  obs_end_date_assign <-
-    obs_end_date[as.numeric(obs_id_assign)] # assigning end date to person
-
-  # outcome table
-
-
-  cohort_definition_list <-
-    c("1") # define element in cohort definition
-  cohort_definition <-
-    sample(cohort_definition_list,
-      round(sample_size * Prevalance, digits = 0),
-      replace = TRUE
-    ) # generate list of cohort definition to the prevalance level
-  subject_id <-
-    sample(id, round(sample_size * Prevalance, digits = 0), replace = FALSE)
-  subject_date <-
-    lapply(subject_id, function(i) {
-      sample(seq(
-        as.Date(obs_start_date_assign[as.numeric(i)]),
-        as.Date(obs_end_date_assign[as.numeric(i)]),
-        by = "day"
-      ), 1)
-    }) # generate list of cohort start date between obs_start and obs_end date
-
-  cohort_start <- do.call("c", subject_date)
-
-  subject_obs_end <-
-    obs_end_date_assign[as.numeric(subject_id)] # subject obs end date for subject with an outcome
-
-
-  # Loop to create cohort_end_date
-  cohort_end <- list()
-  for (i in 1:length(cohort_start)) {
-    cohort_end[i] <- as.character(sample(seq(
-      as.Date(cohort_start[i]),
-      as.Date(subject_obs_end[i]),
-      by = "day"
-    ), 1))
-  }
-
-
-  cohort_end <- unlist(cohort_end)
-
-
-
-
-
+    sample(seq(as.Date("2005-01-01"), as.Date("2010-01-01"), by = "day"),
+           sample_size, replace = TRUE) # start date for the period
+  obs_end_date <- obs_start_date + lubridate::days(sample(1:1000,
+                                               sample_size, replace = TRUE))
   if (is.null(person)) {
     person <- tibble::tibble(
       person_id = id,
@@ -156,24 +106,43 @@ generate_mock_incidence_prevalence_db <- function(person = NULL,
 
   if (is.null(observation_period)) {
     observation_period <- tibble::tibble(
-      observation_period_id = obs_id_assign,
+      observation_period_id = id,
       person_id = id,
-      observation_period_start_date = as.Date(obs_start_date_assign),
-      observation_period_end_date = as.Date(obs_end_date_assign)
+      observation_period_start_date = obs_start_date,
+      observation_period_end_date = obs_end_date
     )
+  }
   }
 
   if (is.null(outcome)) {
-    outcome <- tibble::tibble(
-      cohort_definition_id = cohort_definition,
-      subject_id = subject_id,
-      cohort_start_date = cohort_start,
-      ## cohort_end_date = as.Date(cohort_end)
-      cohort_end_date = cohort_start
-    )
+  # outcome table
+  # note, only one outcome cohort
+  subject_id <- sample(person$person_id,
+                       round(nrow(person) * out_pre, digits = 0),
+                       replace = FALSE)
+
+  outcome<- observation_period %>%
+    rename("subject_id"="person_id") %>%
+    filter(.data$subject_id %in% .env$subject_id) %>%
+    mutate(obs_days=as.numeric(difftime(.data$observation_period_end_date,
+                                        .data$observation_period_start_date,
+                               units= "days")))  %>%
+    mutate(days_to_outcome=round(runif(length(.env$subject_id),
+                                 min = 1,
+                                 max=.data$obs_days))) %>%
+    mutate(cohort_start_date = .data$observation_period_start_date +
+             .data$days_to_outcome) %>%
+    mutate(cohort_end_date = .data$cohort_start_date +
+             lubridate::days(1)) %>%
+    select("subject_id",
+           "cohort_start_date",
+           "cohort_end_date") %>%
+    mutate(cohort_definition_id = c("1")) %>%
+    relocate(cohort_definition_id)
+
   }
 
-  # into in-memory databse
+  # into in-memory database
   db <- DBI::dbConnect(duckdb::duckdb(), ":memory:")
 
   DBI::dbWithTransaction(db, {
