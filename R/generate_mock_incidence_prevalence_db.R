26c6dedd41b524f33f8a675f69356b26e734a380
edward-burn <9583964+edward-burn@users.noreply.github.com>
2022-09-13 09:25:48 +0200

    tweaks to mock db

--- R/generate_mock_incidence_prevalence_db.R
+++ R/generate_mock_incidence_prevalence_db.R
@@ -88,9 +88,21 @@ generate_mock_incidence_prevalence_db <- function(person = NULL,
   checkmate::assertDate(as.Date(earliest_date_of_birth), null.ok = TRUE)
   checkmate::assertDate(as.Date(latest_date_of_birth), null.ok = TRUE)
   checkmate::assertDate(as.Date(earliest_observation_start_date), null.ok = TRUE)
-  checkmate::assertDate(as.Date(latest_observation_start_date),  null.ok = TRUE)
+  checkmate::assertDate(as.Date(latest_observation_start_date), null.ok = TRUE)
   checkmate::assert_int(min_days_to_observation_end, lower = 1, null.ok = TRUE)
   checkmate::assert_int(max_days_to_observation_end, lower = 1, null.ok = TRUE)
+  if (!is.null(latest_date_of_birth) &
+    !is.null(earliest_date_of_birth)) {
+    checkmate::assertTRUE(latest_date_of_birth >= earliest_date_of_birth)
+  }
+  if (!is.null(earliest_observation_start_date) &
+    !is.null(latest_observation_start_date)) {
+    checkmate::assertTRUE(latest_observation_start_date >= earliest_observation_start_date)
+  }
+  if (!is.null(min_days_to_observation_end) &
+    !is.null(max_days_to_observation_end)) {
+    checkmate::assertTRUE(max_days_to_observation_end >= min_days_to_observation_end)
+  }
   checkmate::reportAssertions(collection = errorMessage)
 
   set.seed(seed)
@@ -100,92 +112,70 @@ generate_mock_incidence_prevalence_db <- function(person = NULL,
     id <- as.character(seq(1:sample_size))
     # person gender
     gender_id <- sample(c("8507", "8532"),
-                        sample_size,
-                        replace = TRUE
+      sample_size,
+      replace = TRUE
     )
 
-  #Define earliest possible date of birth for person table
-  if (is.null(earliest_date_of_birth))
-  {
-    earliest_date_of_birth = as.Date("1920-01-01")
+    # Define earliest possible date of birth for person table
+    if (is.null(earliest_date_of_birth)) {
+      earliest_date_of_birth <- as.Date("1920-01-01")
+    }
+    # Define latest possible date of birth for person table
+    if (is.null(latest_date_of_birth)) {
+      latest_date_of_birth <- as.Date("2000-01-01")
+    }
 
-  } else {
-    earliest_date_of_birth = as.Date(earliest_date_of_birth)
-  }
-  #Define latest possible date of birth for person table
-  if (is.null(latest_date_of_birth))
-  {
-    latest_date_of_birth = as.Date("2000-01-01")
-
-  } else {
-    latest_date_of_birth = as.Date(max(as.Date(latest_date_of_birth),as.Date(earliest_date_of_birth)))
-  }
-
-
-  DOB <- sample(seq(
-    as.Date(earliest_date_of_birth),
-    as.Date(latest_date_of_birth),
-    by = "day"
-  ),
-  sample_size,
-  replace = TRUE)
-  # year, month, day
-  DOB_year <- as.numeric(format(DOB, "%Y"))
-  DOB_month <- as.numeric(format(DOB, "%m"))
-  DOB_day <- as.numeric(format(DOB, "%d"))
-
-  # observation_period table
-  # create a list of observational_period_id
-
-  #define earliest observation start date for obs table
-  if (is.null(earliest_observation_start_date))
-  {
-    earliest_observation_start_date = as.Date(max(as.Date("2005-01-01"), latest_date_of_birth))
-
-  } else {
-    earliest_observation_start_date = as.Date(max(
-      as.Date(earliest_observation_start_date),
-      latest_date_of_birth
-    ))
-  }
-  #define latest observation start date for obs table
-  if (is.null(latest_observation_start_date))
-  {
-    latest_observation_start_date = as.Date(max(as.Date("2010-01-01"), latest_date_of_birth))
-
-  } else {
-    latest_observation_start_date = as.Date(max(
-      as.Date(latest_observation_start_date),
-      latest_date_of_birth
-    ))
-  }
-  obs_start_date <-
-    sample(seq(
-      as.Date(earliest_observation_start_date),
-      as.Date(latest_observation_start_date),
+    DOB <- sample(seq(
+      as.Date(earliest_date_of_birth),
+      as.Date(latest_date_of_birth),
       by = "day"
     ),
     sample_size,
-    replace = TRUE) # start date for the period
-
-
-  #define min and max day to observation end
-
-  if (is.null(min_days_to_observation_end)) {
-    min_days_to_observation_end = 1
-  }
-  if (is.null(max_days_to_observation_end)) {
-    max_days_to_observation_end = 1000
-  }
-
-  obs_end_date <-
-    obs_start_date + lubridate::days(
-      sample(
-        min_days_to_observation_end:max_days_to_observation_end,
-        sample_size,
-        replace = TRUE
-      )
+    replace = TRUE
     )
+    # year, month, day
+    DOB_year <- as.numeric(format(DOB, "%Y"))
+    DOB_month <- as.numeric(format(DOB, "%m"))
+    DOB_day <- as.numeric(format(DOB, "%d"))
+
+    # observation_period table
+    # create a list of observational_period_id
+
+    # define earliest and latest observation start date for obs table
+    # if not specified by user
+    if (is.null(earliest_observation_start_date)) {
+      earliest_observation_start_date <- as.Date("2005-01-01")
+    }
+    if (is.null(latest_observation_start_date)) {
+      latest_observation_start_date <- as.Date("2020-01-01")
+    }
+    obs_start_date <-
+      sample(seq(
+        as.Date(earliest_observation_start_date),
+        as.Date(latest_observation_start_date),
+        by = "day"
+      ),
+      sample_size,
+      replace = TRUE
+      ) # start date for the period
+
+
+    # define min and max day to observation end
+    if (is.null(min_days_to_observation_end)) {
+      min_days_to_observation_end <- 1
+    }
+    if (is.null(max_days_to_observation_end)) {
+      max_days_to_observation_end <- 1000
+    }
+
+    obs_end_date <-
+      obs_start_date + lubridate::days(
+        sample(
+          min_days_to_observation_end:max_days_to_observation_end,
+          sample_size,
+          replace = TRUE
+        )
+      )
     if (is.null(person)) {
       person <- tibble::tibble(
         person_id = id,
@@ -207,45 +197,46 @@ generate_mock_incidence_prevalence_db <- function(person = NULL,
   }
 
   if (is.null(outcome)) {
+    if (is.null(age_beta) || is.null(gender_beta) || is.null(intercept)) {
+      # outcome table
+      # note, only one outcome cohort
+      subject_id <- sample(person$person_id,
+        round(nrow(person) * out_pre, digits = 0),
+        replace = FALSE
+      )
 
-    if(is.null(age_beta)||is.null(gender_beta)||is.null(intercept)) {
-    # outcome table
-    # note, only one outcome cohort
-    subject_id <- sample(person$person_id,
-                         round(nrow(person) * out_pre, digits = 0),
-                         replace = FALSE
-    )
-
-    outcome <- observation_period %>%
-      dplyr::rename("subject_id" = "person_id") %>%
-      dplyr::filter(.data$subject_id %in% .env$subject_id) %>%
-      dplyr::mutate(obs_days = as.numeric(difftime(.data$observation_period_end_date,
-                                                   .data$observation_period_start_date,
-                                                   units = "days"
-      ))) %>%
-      dplyr::mutate(days_to_outcome = round(stats::runif(length(.env$subject_id),
-                                                         min = 1,
-                                                         max = .data$obs_days
-      ))) %>%
-      dplyr::mutate(cohort_start_date = .data$observation_period_start_date +
-                      .data$days_to_outcome) %>%
-      dplyr::mutate(cohort_end_date = .data$cohort_start_date +
-                      lubridate::days(1)) %>%
-      dplyr::select(
-        "subject_id",
-        "cohort_start_date",
-        "cohort_end_date"
-      ) %>%
-      dplyr::mutate(cohort_definition_id = c("1")) %>%
-      dplyr::relocate(.data$cohort_definition_id)
-    }
-    else {   # outcome table
-      # calulate outcome
+      outcome <- observation_period %>%
+        dplyr::rename("subject_id" = "person_id") %>%
+        dplyr::filter(.data$subject_id %in% .env$subject_id) %>%
+        dplyr::mutate(obs_days = as.numeric(difftime(.data$observation_period_end_date,
+          .data$observation_period_start_date,
+          units = "days"
+        ))) %>%
+        dplyr::mutate(days_to_outcome = round(stats::runif(length(.env$subject_id),
+          min = 1,
+          max = .data$obs_days
+        ))) %>%
+        dplyr::mutate(cohort_start_date = .data$observation_period_start_date +
+          .data$days_to_outcome) %>%
+        dplyr::mutate(cohort_end_date = .data$cohort_start_date +
+          lubridate::days(1)) %>%
+        dplyr::select(
+          "subject_id",
+          "cohort_start_date",
+          "cohort_end_date"
+        ) %>%
+        dplyr::mutate(cohort_definition_id = c("1")) %>%
+        dplyr::relocate(.data$cohort_definition_id)
+    } else { # outcome table
+      # calculate outcome
 
       person_cal <-
-        person %>% dplyr::mutate(age =  floor(as.numeric(difftime(
-          Sys.Date(), DOB, units = "weeks"# calculating age from dob
-        )) / 52.25)) %>% dplyr::mutate_at("age", ~ (scale(.) %>% as.vector)) %>% # standardizing age variable to have mean 0
+        person %>%
+        dplyr::mutate(age = floor(as.numeric(difftime(
+          Sys.Date(), DOB,
+          units = "weeks" # calculating age from dob
+        )) / 52.25)) %>%
+        dplyr::mutate_at("age", ~ (scale(.) %>% as.vector())) %>% # standardizing age variable to have mean 0
         dplyr::mutate(male_flag = ifelse(gender_id == "8507", 1, 0)) %>% # binary variable for gender
         dplyr::mutate(
           pre = exp(
@@ -257,11 +248,13 @@ generate_mock_incidence_prevalence_db <- function(person = NULL,
           )
         ) %>% # outcome pre calculator for each person in the person table
         dplyr::mutate(outcome_flag = sapply(.data$pre, function(x) {
-          binary_flag <- stats::rbinom(n = 1,
-                                       size = 1,
-                                       prob = min(x, 1))
+          binary_flag <- stats::rbinom(
+            n = 1,
+            size = 1,
+            prob = min(x, 1)
+          )
           return(binary_flag)
-        }))# generate binary outcome with adjust_pre
+        })) # generate binary outcome with adjust_pre
 
       outcome_1 <-
         person_cal %>% dplyr::filter(.data$outcome_flag == 1) # subset of person with outcome
@@ -271,24 +264,25 @@ generate_mock_incidence_prevalence_db <- function(person = NULL,
         dplyr::rename("subject_id" = "person_id") %>%
         dplyr::filter(.data$subject_id %in% .env$subject_id) %>%
         dplyr::mutate(obs_days = as.numeric(difftime(.data$observation_period_end_date,
-                                                     .data$observation_period_start_date,
-                                                     units = "days"
+          .data$observation_period_start_date,
+          units = "days"
         ))) %>%
         dplyr::mutate(days_to_outcome = round(stats::runif(length(.env$subject_id),
-                                                           min = 1,
-                                                           max = .data$obs_days
+          min = 1,
+          max = .data$obs_days
         ))) %>%
         dplyr::mutate(cohort_start_date = .data$observation_period_start_date +
-                        .data$days_to_outcome) %>%
+          .data$days_to_outcome) %>%
         dplyr::mutate(cohort_end_date = .data$cohort_start_date +
-                        lubridate::days(1)) %>%
+          lubridate::days(1)) %>%
         dplyr::select(
           "subject_id",
           "cohort_start_date",
           "cohort_end_date"
         ) %>%
         dplyr::mutate(cohort_definition_id = c("1")) %>%
-        dplyr::relocate(.data$cohort_definition_id)}
+        dplyr::relocate(.data$cohort_definition_id)
+    }
   }
 
 
@@ -298,27 +292,25 @@ generate_mock_incidence_prevalence_db <- function(person = NULL,
 
   DBI::dbWithTransaction(db, {
     DBI::dbWriteTable(db, "person",
-                      person,
-                      overwrite = TRUE
+      person,
+      overwrite = TRUE
     )
   })
   DBI::dbWithTransaction(db, {
     DBI::dbWriteTable(db,
-                      "observation_period",
-                      observation_period,
-                      overwrite = TRUE
+      "observation_period",
+      observation_period,
+      overwrite = TRUE
     )
   })
 
   DBI::dbWithTransaction(db, {
     DBI::dbWriteTable(db, "outcome",
-                      outcome,
-                      overwrite = TRUE
+      outcome,
+      overwrite = TRUE
     )
   })
 
 
   return(db)
 }
-
-
